b875f85e959a67632ea03b2563c73fc35c1fac23
jspgmp <mmriann@gmail.com>
2021-08-20 16:45:19 +0900

    upadate: apply review
    apply review after rubberduck

--- Week #4. byte-degree-html/src/assets/js/main.js
+++ Week #4. byte-degree-html/src/assets/js/main.js
@@ -1,7 +1,17 @@
-// 글쓰기 버튼 페이지 이동 방지
-const $submit = document.querySelector('button[type="submit"]');
-$submit &&
-    $submit.addEventListener("click", e => {
-        e.preventDefault();
-        alert("Great!");
-    });
+// 글쓰기 버튼 페이지 이동 방지(defer를 사용한다면)
+// const $submit = document.querySelector('button[type="submit"]');
+// $submit &&
+//     $submit.addEventListener("click", e => {
+//         e.preventDefault();
+//         alert("Great!");
+//     });
+
+//DOM 객체를 이용한 방식, DOM 로드가 다 됐으면, 다음을 실행해라
+document.addEventListener("DOMContentLoaded", function() { 
+    const $submit = document.querySelector('button[type="submit"]');
+    $submit &&
+        $submit.addEventListener("click", e => {
+            // e.preventDefault();
+            alert("Great!");
+        });
+});
